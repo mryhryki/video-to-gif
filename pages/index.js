afe4314cc967a85c2a5ba97405dd4a6d18f9bbec
mryhryki <mryhryki@gmail.com>
2021-12-29 09:54:50 +0900

    Update author

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ const Home = () => {
         <meta name="twitter:title" content="Video to GIF"/>
         <meta name="twitter:description" content="Convert video to gif on browser. powered by ffmpeg.wasm."/>
         <meta name="twitter:image" content="https://video-to-gif.vercel.app/logo.png"/>
-        <meta name="twitter:site" content="@hyiromori"/>
+        <meta name="twitter:site" content="@mryhryki"/>
         <title>Video to GIF</title>
         <script src="/ffmpeg.min.js"/>
       </Head>
@@ -97,11 +97,11 @@ const Home = () => {
       <footer className={styles.footer}>
         &copy; 2020{' '}
         <a
-          href="https://hyiromori.com/"
+          href="https://mryhryki.com/"
           target="_blank"
           rel="noreferrer noopener"
           className={styles.copyright}
-        >hyiromori</a>
+        >mryhryki</a>
       </footer>
     </>
   )
