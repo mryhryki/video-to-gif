d47a54b107a32598fa735dcf6c1bd0ef68143d4a
mryhryki <mryhryki@gmail.com>
2022-01-03 16:06:37 +0900

    Show history

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,9 @@ import { useEffect, useState } from "react";
 import { gifDataToUrl } from "../lib/buffer_to_url";
 
 const Home = () => {
-  const [file, setFile] = useState(null);
   const [frameRate, setFrameRate] = useState(10);
   const [status, setStatus] = useState(null);
+  const [file, setFile] = useState(null);
   const [gifUrl, setGifUrl] = useState("");
   const { addHistory, histories } = useHistory();
 
@@ -42,7 +42,7 @@ const Home = () => {
       <Head>
         <meta property="og:title" content="Video to GIF"/>
         <meta property="og:description" content="Convert video to gif on browser. powered by ffmpeg.wasm."/>
-        <meta property="og:image" content="https://video-to-gif.vercel.app/logo.png"/>
+        <meta property="og:image" content="https://video-to-gif.vercel.app/logo_1200x1200.png"/>
         <meta property="og:url" content="https://video-to-gif.vercel.app/"/>
         <meta property="og:site_name" content="Video to GIF"/>
         <meta property="og:locale" content="ja-JP"/>
@@ -56,50 +56,62 @@ const Home = () => {
         <script src="/ffmpeg.min.js"/>
       </Head>
 
-      <div className={styles.wrapper}>
-        <main className={styles.content}>
-          <h1 className={styles.centering}>
-            <img className={styles.logo} width="320" src="/logo.png" alt="Video to GIF Logo"/>
-          </h1>
-          {support ? (
-            <>
-              <div className={styles.centering}>
+      <header className={styles.header}>
+        <img className={styles.logo} height="48" width="48" src="/logo_96x96.png" alt="Logo"/>
+        <h1>Video to GIF</h1>
+      </header>
+      <main className={styles.content}>
+        {support ? (
+          <>
+            <input
+              type="file"
+              onChange={(event) => setFile(event.target.files[0])}
+              accept="video/*"
+            />
+            <div className={styles.centering}>
+              <h2>Frame Rate</h2>
+              <div>
                 <input
-                  type="file"
-                  onChange={(event) => setFile(event.target.files[0])}
-                  accept="video/*"
+                  type="range" min="1" max="30" step="1"
+                  value={frameRate}
+                  onChange={(event) => setFrameRate(parseInt(event.target.value, 10))}
                 />
               </div>
-              <div className={styles.centering}>
-                <h2>Frame Rate</h2>
-                <div>
-                  <input
-                    type="range" min="1" max="30" step="1"
-                    value={frameRate}
-                    onChange={(event) => setFrameRate(parseInt(event.target.value, 10))}
-                  />
-                </div>
-                <div>{frameRate}FPS</div>
-              </div>
-              <div className={styles.centering}>
-                <button className={styles.button} onClick={transcode} disabled={status != null || file == null}>
-                  Convert to GIF
-                </button>
-              </div>
-              {status != null && (
-                <section className={styles.centering}>
-                  <div className={styles.status}>{status}</div>
-                </section>
-              )}
-              {gifUrl !== "" && (
-                <section className={styles.output}>
-                  <img className={styles.gif} alt="Output GIF" src={gifUrl}/>
-                  <a className={styles.button} href={gifUrl} download={`${file?.name ?? "image"}.gif`}>Download GIF</a>
-                </section>
-              )}
-            </>
-          ) : <Unsupported/>}
-        </main>
+              <div>{frameRate}FPS</div>
+            </div>
+            <div className={styles.centering}>
+              <button className={styles.button} onClick={transcode} disabled={status != null || file == null}>
+                Convert to GIF
+              </button>
+            </div>
+            {status != null && (
+              <section className={styles.centering}>
+                <div className={styles.status}>{status}</div>
+              </section>
+            )}
+            {gifUrl !== "" && (
+              <section className={styles.output}>
+                <img className={styles.gif} alt="Output GIF" src={gifUrl}/>
+                <a className={styles.button} href={gifUrl} download={`${file?.name ?? "image"}.gif`}>Download GIF</a>
+              </section>
+            )}
+          </>
+        ) : <Unsupported/>}
+      </main>
+
+      <div className={styles.history}>
+        {histories.map((history) => (
+          <div key={history.datetime} className={styles.historyCard}>
+            <div className={styles.historyDatetime}>
+              {history.datetime}
+            </div>
+            <img
+              className={styles.historyGif}
+              alt={`Converted at ${history.datetime}`}
+              src={gifDataToUrl(history.gifData, history.datetime)}
+            />
+          </div>
+        ))}
       </div>
 
       <footer className={styles.footer}>
