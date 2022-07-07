90eae60dbbf56e6ebe57a1327815cf7b9f65bb43
mryhryki <mryhryki@gmail.com>
2022-07-08 08:20:46 +0900

    Replace @vitejs/plugin-react-refresh to @vitejs/plugin-react

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import reactPlugin from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
-  plugins: [reactRefresh()],
+  plugins: [reactPlugin()],
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
